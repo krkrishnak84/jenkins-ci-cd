35580d5738e005c35a01f7ce573921267ab1b777
Radhakrishna <kuchi.radhakrishna@gmail.com>
2024-03-23 13:05:53 -0400

    jenkins demo files

---
+++ src/main/java/com/org/jenkins/JenkingsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.org.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkingsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkingsDemoApplication.class, args);
+	}
+
+}
